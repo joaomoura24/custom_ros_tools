ebf8a404092943f5dfe1821230a03f323a7f5689
Chris E. Mower <chris.mower@ed.ac.uk>
2022-03-30 12:27:16 +0100

    Add type hints

--- custom_ros_tools/src/custom_ros_tools/config.py
+++ custom_ros_tools/src/custom_ros_tools/config.py
@@ -7,11 +7,11 @@ import rospkg
 
 rp = rospkg.RosPack()
 
-def ros_package_path(package):
+def ros_package_path(package: str) -> str:
     """Wrapper for rp.get_path - to ease effort when porting to ROS2."""
     return rp.get_path(package)
 
-def replace_package(path):
+def replace_package(path: str) -> str:
     """Returns the absolute path to a file. The path can be given relative to ROS package in the format '{ros_package_name}/path/to/file'."""
     matches = re.findall(r'{.+?}', path)
     if len(matches) > 0:
@@ -21,17 +21,17 @@ def replace_package(path):
         path = path.replace(match, root)
     return path
 
-def load_config(path):
+def load_config(path: str) -> dict:
     """Load config from file."""
     path_ = replace_package(path)
     with open(path_, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     return config
 
-def load_configs(s):
+def load_configs(s: str) -> dict:
     """Load config from string."""
     return yaml.load(f, Loader=yaml.FullLoader)
 
-def config_to_str(config):
+def config_to_str(config: dict) -> str:
     """Convert a configuration to string."""
     return yaml.dump(config)
