0fca4e5dbfced693ebad99f1d08c3974d9343ff0
Chris E. Mower <chris.mower@ed.ac.uk>
2022-03-30 12:42:50 +0100

    Add typing hints

--- custom_ros_tools/src/custom_ros_tools/tf.py
+++ custom_ros_tools/src/custom_ros_tools/tf.py
@@ -2,7 +2,9 @@ import rospy
 import tf2_ros
 import tf_conversions
 import numpy as np
+from numpy.typing import ArrayLike
 from geometry_msgs.msg import TransformStamped
+from typing import Optional, Tuple
 
 class TfInterface:
 
@@ -11,7 +13,13 @@ class TfInterface:
         self.tf_buffer = tf2_ros.Buffer()
         tf2_ros.TransformListener(self.tf_buffer)
 
-    def set_tf(self, parent_frame_id, child_frame_id, pos, orientation=[0, 0, 0, 1], eul_deg=False):
+    def set_tf(self,
+               parent_frame_id: str,
+               child_frame_id: str,
+               pos: ArrayLike,
+               orientation: Optional[ArrayLike] = [0, 0, 0, 1],
+               eul_deg: Optional[bool] = False
+               ):
         """Set the position and orientation of a child frame with respect to a parent frame."""
 
         # Handle euler angles
@@ -27,7 +35,7 @@ class TfInterface:
         # Pack transform message and broadcast
         self.tf_broadcaster.sendTransform(self.pack_tf_msg(parent_frame_id, child_frame_id, pos, orientation))
 
-    def get_tf_msg(self, parent_frame_id, child_frame_id):
+    def get_tf_msg(self, parent_frame_id: str, child_frame_id: str) -> Union[TransformStamped, None]:
         msg = None
         try:
             msg = self.tf_buffer.lookup_transform(parent_frame_id, child_frame_id, rospy.Time())
@@ -35,7 +43,7 @@ class TfInterface:
             rospy.logdebug(f'Did not recieve the frame {child_frame_id} in {parent_frame_id}!')
         return msg
 
-    def get_tf(self, parent_frame_id, child_frame_id):
+    def get_tf(self, parent_frame_id: str, child_frame_id: str) -> Tuple[ArrayLike]:
         """Return position and orientation of child frame with respect to a parent frame."""
         msg = self.get_tf_msg(parent_frame_id, child_frame_id)
         if msg is not None:
@@ -47,7 +55,7 @@ class TfInterface:
         return pos, orientation
 
     @staticmethod
-    def pack_tf_msg(parent_frame_id, child_frame_id, pos, rotation):
+    def pack_tf_msg(parent_frame_id: str, child_frame_id: str, pos: ArrayLike, rotation: ArrayLike) -> TransformStamped:
         msg = TransformStamped()
         msg.header.stamp = rospy.Time.now()
         msg.header.frame_id = parent_frame_id
@@ -62,25 +70,25 @@ class TfInterface:
         return msg
 
     @staticmethod
-    def msg_to_pos(msg):
+    def msg_to_pos(msg: TransformStamped) -> ArrayLike:
         return np.array([getattr(msg.transform.translation, d) for d in 'xyz'])
 
     @staticmethod
-    def msg_to_quat(msg):
+    def msg_to_quat(msg: TransformStamped) -> ArrayLike:
         return np.array([getattr(msg.transform.rotation, d) for d in 'xyzw'])
 
     @staticmethod
-    def msg_to_pos_quat(msg):
+    def msg_to_pos_quat(msg: TransformStamped) -> Tuple[Arraylike]:
         return self.msg_to_pos(msg), self.msg_to_quat(msg)
 
     @staticmethod
-    def msg_to_matrix(msg):
+    def msg_to_matrix(msg: TransformStamped) -> ArrayLike:
         p = TfInterface.msg_to_pos(msg)
         q = TfInterface.msg_to_quat(msg)
         return TfInterface.pos_and_quat_to_matrix(p, q)
 
     @staticmethod
-    def pos_quat_to_matrix(pos, quat=[0,0,0,1]):
+    def pos_quat_to_matrix(pos: ArrayLike, quat: Optional[ArrayLike] = [0,0,0,1]) -> ArrayLike:
         T = tf_conversions.transformations.quaternion_matrix(quat)
         T[:3, 3] = pos
         return T
